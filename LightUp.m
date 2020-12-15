09a67f37320f543babf3709234dfeae2393978fb
GUZE-gwhifw2020 <gwhifw2020@gmail.com>
2020-12-15 13:42:33 +0800

    添加顶角判断
    四个顶角依据情况判断是否为不可放置灯属性

--- LightUp.m
+++ LightUp.m
@@ -3,9 +3,11 @@ classdef LightUp
     %   此处显示详细说明
     
     properties(Constant)
+        % 所有数据禁止修改
+        
         dirU = 1        % 方向常数
-        dirD = 2        % 方向常数
-        dirL = 3        % 方向常数
+        dirD = 3        % 方向常数
+        dirL = 2        % 方向常数
         dirR = 4        % 方向常数
         
         utypeBlc    = -1
@@ -22,10 +24,11 @@ classdef LightUp
         mat         % 题面矩阵
         
         indS4       % 四个方向下标偏移值
+        indA4       % 四个顶角下标偏移值
         
         rowPairs    % 连续空行,cell(height,1),内部(pairsNum * 2)
         colPairs    % 连续空列,cell(width,1),内部(pairsNum * 2)
-  
+        
         blackInd    % 数字黑格下标
         blackDig    % 黑格数字
         
@@ -43,11 +46,16 @@ classdef LightUp
                 luTokenResolve(tokStrArg);
             
             % 四个方向下标偏移值
-            obj.indS4 = [-1 1 -obj.height obj.height];
-           
+            obj.indS4 = zeros(1,4);
+            obj.indS4(obj.dirU) = -1;
+            obj.indS4(obj.dirD) = 1;
+            obj.indS4(obj.dirL) = -obj.height;
+            obj.indS4(obj.dirR) = obj.height;
+            obj.indA4 = reshape([-obj.height obj.height] + [-1;1], [1 4]);
+            
             % 黑格数字
             obj.blackDig = obj.mat(obj.blackInd);
-
+            
             % 生成连续空行/列
             obj.rowPairs = cell(obj.height, 1);
             obj.colPairs = cell(obj.width, 1);
@@ -99,16 +107,38 @@ classdef LightUp
             
             % 已有灯位置 - 布尔型
             b2 = (matS == obj.utypeLamp);
+            % 残留可填位置
+            b3 = (matS == obj.utypeUnn);
             % 可填位置 - 布尔型
-            b1 = b2 | (matS == obj.utypeUnn);
+            b1 = b2 | b3;
             if(nnz(b1) == digit)
                 % 周边可填位置等于数字 -> 可填位置放置灯
                 obj.addLampInd = cat(2, obj.addLampInd, matInd + obj.indS4(b1));
             elseif(nnz(b2) == digit)
                 % 周围灯数等于数字 -> 剩余位置设置不可放属性
                 obj.mat(matInd + obj.indS4(~b2)) = obj.utypeNLmp;
+            elseif(nnz(b3) - digit + nnz(b2) == 1)
+                % 残留可填位置数 - 残留灯数 = 1
+                % 四个顶角判断
+                switch(nnz(b3))
+                    case{4}
+                        obj.mat(matInd + obj.indA4) = obj.utypeNLmp;
+                    case{3}
+                        % 残留位置中空元素
+                        indDir = find(~b3,1,'first');
+                        % 转换为待写入两个方向
+                        indDir = mod(indDir + [0 1],4) + 1;
+                        obj.mat(matInd + obj.indA4(indDir)) = obj.utypeNLmp;
+                    case{2}
+                        % 不能为水平型，即不能为(1,3)或(2,4)
+                        if(any(b3 & b3([4 1 2 3])))
+                            % 待写入一个方向
+                            indDir = find(b3,1,'last') - 1;
+                            obj.mat(matInd + obj.indA4(indDir)) = obj.utypeNLmp;
+                        end
+                end
             end
-
+            
         end
         
         function obj = addLamp(obj)
@@ -158,6 +188,8 @@ classdef LightUp
             % 绘制点亮区域
             spy(obj.mat == obj.utypeLit, 30, '.y');
             
+            % 绘制不可填充
+            spy(obj.mat == obj.utypeNLmp,'xm');
             % 绘制网格
             
             
