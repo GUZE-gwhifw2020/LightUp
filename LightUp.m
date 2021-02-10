91dc6abf4e92219e7b9f4427ef703a7b24c3c818
GUZE-gwhifw2020 <gwhifw2020@gmail.com>
2021-02-10 22:27:48 +0800

    Updata LightUp.m - 空行列存储方式改善+修正判断错误
    rowPairs   % 连续空行，大小matrix(3, pairsNum)
    colPairs    % 连续空列，大小matrix(3, pairsNum)
    
    添加Lamp的判断准则修缮

--- LightUp.m
+++ LightUp.m
@@ -26,8 +26,11 @@ classdef LightUp
         indS4       % 四个方向下标偏移值
         indA4       % 四个顶角下标偏移值
         
-        rowPairs    % 连续空行,cell(height,1),内部(pairsNum * 2)
-        colPairs    % 连续空列,cell(width,1),内部(pairsNum * 2)
+        rowPairs    % 连续空行，大小matrix(3, pairsNum)
+        colPairs    % 连续空列，大小matrix(3, pairsNum)
+        
+        rowPairInd  % 非黑格空行坐标，大小matrix(height, width)
+        colPairInd  % 非黑格空行坐标，大小matrix(height, width)
         
         blackInd    % 数字黑格下标
         blackDig    % 黑格数字
@@ -55,9 +58,7 @@ classdef LightUp
             % 黑格数字
             obj.blackDig = obj.mat(obj.blackInd);
             
-            % 生成连续空行/列
-            obj.rowPairs = cell(obj.height, 1);
-            obj.colPairs = cell(obj.width, 1);
+            % 生成连续空行/列坐标
             obj = obj.initRCPairs();
 
         end
@@ -65,37 +66,52 @@ classdef LightUp
         function obj = Genesis(obj)
             %GENESIS 求解工程
             
-            for iter = 1:1
+            for iter = 1:3
                 % 每一个数字黑格处理
                 for ind = 1:length(obj.blackInd)
                     obj = obj.checkBlack(ind);
                 end
                 
                 % 每一个行列条带处理
-                for ii = 2:obj.width-1
-                    for kk = 1:size(obj.colPairs{ii},1)
-                        obj = obj.checkColPairs(ii,kk);
-                    end
+                for ii = 1:size(obj.rowPairs, 2)
+                    obj = obj.checkColPairs(ii);
                 end
-                for ii = 2:obj.height-1
-                    for kk = 1:size(obj.rowPairs{ii},1)
-                        obj = obj.checkRowPairs(ii,kk);
-                    end
+                for ii = 1:size(obj.colPairs, 2)
+                    obj = obj.checkRowPairs(ii);
                 end
             end
         end
         
         function obj = initRCPairs(obj)
-            %INITRCPAIRS 初始化连续空行/列
-            for ii = 1:obj.width
-                cc = diff(obj.mat(:,ii) >= obj.utypeBlc);
-                obj.colPairs{ii} = cat(2, find(cc == -1) + 1,find(cc == 1));
-            end
-            for jj = 1:obj.height
-                cc = transpose(diff(obj.mat(jj,:) >= obj.utypeBlc));
-                obj.rowPairs{jj} = cat(2, find(cc == -1) + 1,find(cc == 1));
-            end
+            %INITRCPAIRS 初始化连续空行/列，和下标矩阵
             
+            % 未确定位置矩阵
+            matUnn = obj.mat == obj.utypeUnn;
+            % 辅助矩阵
+            matTemp = false(size(obj.mat));
+            
+            % 逐列判断
+            for ii = 1:obj.width
+                % 利用1 0 0 1 0差分后确定间隔位置
+                cc = transpose(diff(matUnn(:, ii)));
+                obj.colPairs = cat(2, obj.colPairs, cat(1, find(cc == 1) + 1,find(cc == -1), repmat(ii, [1, nnz(cc == 1)])));
+                matTemp(find(cc == 1) + 1, ii) = true;
+            end
+            % 连续空列下标赋值
+            obj.colPairInd = reshape(cumsum(matTemp(:)), size(obj.mat));
+            obj.colPairInd(~matUnn) = 0;
+            
+            % 辅助矩阵
+            matTemp = false(size(obj.mat'));
+            % 逐行判断
+            for jj = 1:obj.height
+                cc = diff(matUnn(jj,:));
+                obj.rowPairs = cat(2, obj.rowPairs, cat(1, find(cc == 1) + 1,find(cc == -1), repmat(jj, [1, nnz(cc == 1)])));
+                matTemp(find(cc == 1) + 1, jj) = true;
+            end
+            % 连续空行下标赋值
+            obj.rowPairInd = reshape(cumsum(matTemp(:)), size(obj.mat'))';
+            obj.rowPairInd(~matUnn) = 0;
         end
         
         function obj = checkBlack(obj, ind)
@@ -158,12 +174,14 @@ classdef LightUp
                 end
                 % 从pairs中读取连续行列
                 [row, col] = ind2sub([obj.height obj.width], lampInd);
-                % 连续列设置Lit属性
-                indT = find(obj.colPairs{col}(:,2) >= row, 1,'first');
-                obj.mat(obj.colPairs{col}(indT,1):obj.colPairs{col}(indT,2), col) = obj.utypeLit;
-                % 连续行设置Lit属性
-                indT = find(obj.rowPairs{row}(:,2) >= col, 1,'first');
-                obj.mat(row, obj.rowPairs{row}(indT,1):obj.rowPairs{row}(indT,2)) = obj.utypeLit;
+                % 连续列设置Lit属性，从连续空列中查询到列范围
+                span = obj.colPairs(:, obj.colPairInd(lampInd));
+                obj.mat(colon(span(1), span(2)), col) = obj.utypeLit;
+                
+                % 连续行设置Lit属性，从连续空行中查询到行范围
+                span = obj.rowPairs(:, obj.rowPairInd(lampInd));
+                obj.mat(row, colon(span(1), span(2))) = obj.utypeLit;
+                
                 % 自己设置为Lamp属性
                 obj.mat(lampInd) = obj.utypeLamp;
             end
@@ -176,37 +194,83 @@ classdef LightUp
             matS = obj.mat(matInd + obj.indS4);
         end
         
-        function obj = checkColPairs(obj, colInd, ind)
-            %CHECKCOLPAIRS 确定第colInd列的第ind个pair是否更新
-            pair = obj.colPairs{colInd}(ind,1:2);
-            arr = obj.mat(pair(1):pair(2),colInd) == obj.utypeUnn;
-            if(nnz(arr) == 1)
-                obj = obj.addLamp(sub2ind([obj.height obj.width], ...
-                    find(arr, 1, 'first') - 1 + pair(1), colInd));
+        function obj = checkColPairs(obj, ind)
+            %CHECKCOLPAIRS 确定连续空列的第ind组是否更新
+            
+            % Rev: 原先认为只要连续列中只有一个Unn就应该是Lamp
+            % 实际判断准则还需同时考虑连续行
+            % 1. 在行列中均为唯一的Unn
+            % 2. 列中为唯一的Unn，且该列中存在Cross不可能在行中点亮
+            %    也即该行中不存在Unn
+            
+            span = obj.colPairs(1:2, ind);
+            colInd = obj.colPairs(3, ind);
+            
+            % 寻找该空列中Unn类型个数
+            rowInd = find(obj.mat(span(1):span(2),colInd) == obj.utypeUnn);
+            if(length(rowInd) == 1)
+                % 转换为实际下标
+                rowInd = rowInd - 1 + span(1);
+                
+                % 1. 对应连续空行中Unn个数是否为1
+                span = obj.rowPairs(:, obj.rowPairInd(rowInd, colInd));
+                J1 = (nnz(obj.mat(rowInd, span(1):span(2)) == obj.utypeUnn) == 1);
+                
+                % 2. 寻找该空列的Cross所在的行中是否无Unn
+                J2 = false;
+                for rowIndT = find(obj.mat(span(1):span(2), colInd) == obj.utypeNLmp) - 1 + span(1)
+                    spanT = obj.colPairs(1:2, obj.rowPairInd(rowIndT, colInd));
+                    if(all(obj.mat(rowIndT, spanT(1):spanT(2)) ~= obj.utypeUnn))
+                        J2 = true;
+                        break;
+                    end
+                end
+                if(J1 || J2)
+                    obj = obj.addLamp(sub2ind([obj.height obj.width], ...
+                        rowInd, colInd));
+                end
             end
         end
         
-        function obj = checkRowPairs(obj, rowInd, ind)
-            %CHECKROWPAIRS 确定第rowInd行的第ind个pair是否更新
-            pair = obj.rowPairs{rowInd}(ind,1:2);
-            arr = obj.mat(rowInd, pair(1):pair(2)) == obj.utypeUnn;
-            if(nnz(arr) == 1)
-                obj = obj.addLamp(sub2ind([obj.height obj.width], ...
-                    rowInd, find(arr, 1, 'first') - 1 + pair(1)));
+        function obj = checkRowPairs(obj, ind)
+            %CHECKROWPAIRS 确定连续空行的第ind组是否更新
+
+            span = obj.rowPairs(1:2, ind);
+            rowInd = obj.rowPairs(3, ind);
+            
+            % 寻找空行中Unn个数
+            colInd = find(obj.mat(rowInd, span(1):span(2)) == obj.utypeUnn);
+            if(length(colInd) == 1)
+                % 转换为实际下标
+                colInd = colInd - 1 + span(1);
+                
+                % 1. 对应列中Unn个数是否为1
+                span = obj.colPairs(:, obj.colPairInd(rowInd, colInd));
+                J1 = (nnz(obj.mat(span(1):span(2), colInd) == obj.utypeUnn) == 1);
+                
+                % 2. 寻找该空行的Cross所在列中是否无Unn
+                J2 = false;
+                for colIndT = find(obj.mat(rowInd, span(1):span(2)) == obj.utypeNLmp) - 1 + span(1)
+                    spanT = obj.rowPairs(1:2, obj.colPairInd(rowInd, colIndT));
+                    if(all(obj.mat(spanT(1):spanT(2), colIndT) ~= obj.utypeUnn))
+                        J2 = true;
+                    end
+                end
+                
+                if(J1 || J2)
+                    obj = obj.addLamp(sub2ind([obj.height obj.width], ...
+                        rowInd, colInd));
+                end
+                
             end
+            
+            
         end
         
         function Display(obj)
-            % 关闭已有图窗
-            try
-                close('LightUp')
-            catch ME
-                if(~strcmp(ME.identifier, 'MATLAB:close:WindowNotFound'))
-                    rethrow(ME)
-                end
-            end
             % 新建图窗
-            h = figure('Name', 'LightUp');
+            h = figure('Name', 'LightUp', 'NumberTitle', 'off');
+            clf
             hold on
             
             % 绘制黑格
